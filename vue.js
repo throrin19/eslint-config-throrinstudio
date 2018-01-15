c0a304ecc293fbfb3ebf029a0b330d25c96ea98b
Benjamin Besse <benjamin.besse02@gmail.com>
2018-01-15 11:20:01 +0100

    fix problem with parsers

--- vue.js
+++ vue.js
@@ -3,6 +3,7 @@ const base = require('./index');
 module.exports = {
     parser  : "vue-eslint-parser",
     parserOptions : {
+        "parser": "babel-eslint",
         "sourceType": "module",
         "ecmaVersion": 2017,
         "ecmaFeatures": {
