7f3fb2ed2a2413d8b2bed5ef2946d2e2a5fa486e
Benjamin Besse <benjamin.besse02@gmail.com>
2018-12-20 10:28:42 +0100

    remove no-v-html rule

--- rules/vue/html.js
+++ rules/vue/html.js
@@ -6,5 +6,6 @@ module.exports = {
         }],
         'vue/html-indent' : ["error", 4],
         'vue/singleline-html-element-content-newline' : 'off',
+        'vue/no-v-html' : 'off',
     },
 };
